14b012be1b70a7c81de6b789649b2b0ac77a0386
AlexAndrushkiv <aandrushkiv@gmail.com>
2021-10-26 18:12:18 +0300

    added hw-2

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('error');
+// alert('error');
+let a = 6;
+let m = 'Hello';
+
+console.log(a);
+
+let inputIn = document.querySelector('input-in');
+let button = document.querySelector('btn');
+
+button.onclick = function(params) {
+   
+}
