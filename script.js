b8c9b4786c922d7dcb8fb6246a42c1b24f483f6f
AlexAndrushkiv <aandrushkiv@gmail.com>
2021-10-28 18:19:55 +0300

    added hw#3

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-console.log('error');
-// alert('error');
-let a = 6;
-let m = 'Hello';
+// console.log('error');
+// // alert('error');
+// let a = 6;
+// let m = 'Hello';
 
-console.log(a);
+// console.log(a);
 
-let inputIn = document.querySelector('input-in');
-let button = document.querySelector('btn');
+// let inputIn = document.querySelector('.input-in');
+// let button = document.querySelector('.btn');
 
-button.onclick = function(params) {
+
+// button.onclick = () => {
+
+//   let s = +inputIn.value;
+//    console.log(s - 10);
+//    inputIn.value = ''; //очистить поле input 
+
+// }
+
+// if,else,switch 2.0
+
+/**
+ * При нажатии кнопки .b-1 срабатывает функция f1. Функция должна прочитать содержимое .i-1 и сравнить его с числом 4 (сравнение ==). Результат сравнения - true или false выведите в .out-1.
+ */
+
+// const button = document.querySelector('.btn');
+// const input = document.querySelector('.input-in');
+
+// button.onclick = () => {
+//    let num = +input.value;
+
+//    if (num == 4) console.log(true);
+//    else console.log(false);
+// }
+
+/**
+ * Даны две переменные a21 и a22. При нажатии кнопки .b-2, запускается функция f2. Функция должна сравнить переменные с помощью if else и вывести в .out-2 число, которое больше. Вариант равенства переменных не рассматриваем.
+ */
+
+let a21 = 5;
+let a22 = 7;
+
+function f2() {
+   let input = document.querySelector('.input-in');
    
 }
+let button = document.querySelector('.btn').onclick = f2;
